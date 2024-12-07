a766fe9e6ae3313997eaf4727e6b9b4358602aac
Sandeep K <sandeep@priyarajaelectronics.com>
2024-12-07 18:25:03 +0530

    updated the add group Ui

--- Client/src/Pages/MainPage/Home.jsx
+++ Client/src/Pages/MainPage/Home.jsx
@@ -1,25 +1,73 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import SideBar from "../HelperPages/SideBar";
-import ChatSection from "../HelperPages/ChatSection";
-import AddUser from "../Popups/AddUser"
+// import ChatSection from "../HelperPages/ChatSection";
+import AddUser from "../Popups/AddUser";
+import AddGroup from "../Popups/AddGroup";
 const Home = () => {
+  const [typeOfPopup, setTypeOfPopup] = useState("");
+  const PopupComponents = [
+    {
+      title: "addUser",
+      component: (
+        <AddUser
+          onCancel={() => {
+            setTypeOfPopup("");
+          }}
+        />
+      ),
+    },
+    {
+      title: "addGroup",
+      component: (
+        <AddGroup
+          onCancel={() => {
+            setTypeOfPopup("");
+          }}
+        />
+      ),
+    },
+  ];
+
+  const ReturnComponent = (title) => {
+    let comp = PopupComponents.find((ele) => {
+      return ele.title === title;
+    });
+    if (!comp) {
+      return <></>;
+    } else {
+      return comp.component;
+    }
+  };
   return (
     <div className="flex pt-[4.5rem] ">
       {/* sidebar */}
       <div>
-        <SideBar onAddUser={()=>
-          {
-            // 
-          }
-        }/>
+        <SideBar
+          onAddUser={() => {
+            setTypeOfPopup("addUser");
+          }}
+          onAddGroup={() => {
+            setTypeOfPopup("addGroup");
+          }}
+        />
       </div>
       {/* chat Page */}
       <div>
-        <ChatSection />
+        {
+          <div className="w-[calc(100vw-24rem)] h-[calc(100vh-4.5rem)] bg-blue-200 flex flex-col justify-center items-center">
+            <h1 className="text-4xl font-semibold text-slate-600">
+              Start Chatting
+            </h1>
+            <p className="mt-4 text-base font-semibold text-gray-500">
+              Select some to chat
+            </p>
+          </div>
+        }
+        {/* <ChatSection /> */}
       </div>
       {
         // popups
-        <AddUser/>
+        ReturnComponent(typeOfPopup)
       }
     </div>
   );
